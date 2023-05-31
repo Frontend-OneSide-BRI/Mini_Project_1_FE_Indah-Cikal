2d41118c43a4964de80cc3129d284ab889c20f8a
unknown <indahcikalao@gmail.com>
2023-05-31 23:08:33 +0700

    fix styling

--- Assets/Scripts/BestArtwork.js
+++ Assets/Scripts/BestArtwork.js
@@ -1,21 +1,17 @@
 import data from "./data.js";
 
 const content = document.getElementById("filtered-content");
+const modal = document.getElementById("modal-container");
 
-function filtered(category) {
-  data
-    .filter((item) => item.category === category)
-    .map(
-      (item) =>
-        (content.innerHTML += `
+function renderCard(item) {
+  content.innerHTML += `
           <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 py-2">
-            <a href="#">
+            <a href="#" data-bs-toggle="modal" data-bs-target="#modal-${item.id}">
               <div class="container-card rounded-4">
                 <img class="image" src="${item.src}" />
                 <div class="overlay">
                   <div class="text">
-                    <h5>${item.title}</h5>
-                    <p>${item.category}</p>
+                    <h5 class="fw-bold">${item.title}</h5>
                     <p class="more-info">
                       Learn more <i class="fa-solid fa-arrow-right"></i>
                     </p>
@@ -24,8 +20,25 @@ function filtered(category) {
               </div>
             </a>
           </div>
-            `)
-    );
+            `;
+  modal.innerHTML += `
+        <div class="modal fade" id="modal-${item.id}" >
+          <div class="modal-dialog modal-dialog-centered ">
+            <div class="modal-content rounded-4">
+              <div class="modal-body text-center p-4">
+                <h5 class="fw-bold">${item.title}</h5>
+                <p> Categorized as Our ${item.category}</p>
+                <img class="image-modal rounded-4" src="${item.src}" />
+              </div>
+            </div>
+          </div>
+        </div>`;
+}
+
+function filtered(category) {
+  data
+    .filter((item) => item.category === category)
+    .map((item) => renderCard(item));
 }
 
 const bestArtwork = document.querySelector(".best-artwork #filtered-content");
--- Assets/Scripts/Galery.js
+++ Assets/Scripts/Galery.js
@@ -1,52 +1,47 @@
 import data from "./data.js";
 
 const content = document.getElementById("filtered-content");
+const modal = document.getElementById("modal-container");
+
+function renderCard(item) {
+  content.innerHTML += `
+          <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 py-2">
+            <a href="#" data-bs-toggle="modal" data-bs-target="#modal-${item.id}">
+              <div class="container-card rounded-4">
+                <img class="image" src="${item.src}" />
+                <div class="overlay">
+                  <div class="text">
+                    <h5 class="fw-bold">${item.title}</h5>
+                    <p class="more-info">
+                      Learn more <i class="fa-solid fa-arrow-right"></i>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+            `;
+  modal.innerHTML += `
+        <div class="modal fade" id="modal-${item.id}" >
+          <div class="modal-dialog modal-dialog-centered ">
+            <div class="modal-content rounded-4">
+              <div class="modal-body text-center p-4">
+                <h5 class="fw-bold">${item.title}</h5>
+                <p> Categorized as Our ${item.category}</p>
+                <img class="image-modal rounded-4" src="${item.src}" />
+              </div>
+            </div>
+          </div>
+        </div>`;
+}
 
 function filtered(category) {
   content.innerHTML = "";
   category === "All"
-    ? data.map(
-        (item) =>
-          (content.innerHTML += `
-          <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 py-2">
-            <a href="#">
-              <div class="container-card rounded-4">
-                <img class="image" src="${item.src}" />
-                <div class="overlay">
-                  <div class="text">
-                    <h5>${item.title}</h5>
-                    <p class="more-info">
-                      Learn more <i class="fa-solid fa-arrow-right"></i>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-            `)
-      )
+    ? data.map((item) => renderCard(item))
     : data
         .filter((item) => item.category === category)
-        .map(
-          (item) =>
-            (content.innerHTML += `
-          <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 py-2">
-            <a href="#">
-              <div class="container-card rounded-4">
-                <img class="image" src="${item.src}" />
-                <div class="overlay">
-                  <div class="text">
-                    <h5>${item.title}</h5>
-                    <p class="more-info">
-                      Learn more <i class="fa-solid fa-arrow-right"></i>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-            `)
-        );
+        .map((item) => renderCard(item));
 }
 
 const allPost = document.getElementById("all-post");
